392f9cb370aceb3d721e255b51f0b2f9fb6b10a5
Ri <ri.uni.9.kr@gmail.com>
2026-05-19 23:52:12 +0200

    Added selenium library for working with whatsapp
    Added .gitignore and some more sections in it

--- com/whatsapp/cli/messaging/APP.java
+++ com/whatsapp/cli/messaging/APP.java
@@ -1,7 +1,8 @@
+package com.whatsapp.cli.messaging;
 import automation.OpenWhatsapp;
 
 public class APP{
 	public static void main(String[] args){
-		OpenWhatsapp.openwhatsapp();
+		OpenWhatsapp.openWhatsapp();
 	}
 }
